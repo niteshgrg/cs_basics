e26f526dac7316b642b0a2e031847dcddc1f8790
Nitesh Garg <nitesh.garg@bounceshare.com>
2020-05-22 17:20:01 +0530

    Optimise to o(n)

--- leet code/sortByFrequencies.js
+++ leet code/sortByFrequencies.js
@@ -4,24 +4,30 @@
  */
 var frequencySort = function(s) {
     let frequencyAvailableHashMap = {};
+    let maxCount = 0;
     let returnString = "";
     
     for(let i of s) {
         if(i in frequencyAvailableHashMap) {
-            frequencyAvailableHashMap[i] += 1; 
+            frequencyAvailableHashMap[i] += 1;
         } else {
             frequencyAvailableHashMap[i] = 1; 
         }
+        if(frequencyAvailableHashMap[i] > maxCount) {
+            maxCount = frequencyAvailableHashMap[i];
+        }
     }
     
-    var sortable = Object.entries(frequencyAvailableHashMap);
-
-    sortable.sort(function(a, b) {
-        return b[1] - a[1];
-    });
+    let frequencyArray = new Array(maxCount + 1).fill().map(x => []);
+     
+    for(let i in frequencyAvailableHashMap) {
+        frequencyArray[frequencyAvailableHashMap[i]].push(i);
+    }
     
-    for(let i of sortable) {
-        returnString += i[0].repeat(i[1]);
+    for(let i = frequencyArray.length - 1; i > 0; i--) {
+        for(let j = 0; j < frequencyArray[i].length; j++) {
+            returnString += frequencyArray[i][j].repeat(i);
+        }
     }
     
     return returnString;
