d3a68343d06d8594668f72dc86e36684bbd8373e
Nitesh Garg <nitesh.garg@bounceshare.com>
2020-06-03 13:15:15 +0530

    Fix recommendations in minpath

--- leet code/minPath.js
+++ leet code/minPath.js
@@ -1,22 +1,38 @@
-var minPathSum = function(grid) {
-    let rowCount = grid.length;
-    let columnCount = rowCount > 0 ? grid[0].length : 0;
-    let minArray = new Array(rowCount).fill(Number.MAX_SAFE_INTEGER).map(() => new Array(columnCount).fill(Number.MAX_SAFE_INTEGER));
-    for(let i = rowCount-1; i >=0; i--) {
-        for(j = columnCount-1; j>=0; j--){
-            if((j < columnCount - 1) && (i  < rowCount - 1)) {
-                grid[i][j] = grid[i][j] + Math.min(grid[i][j+1], grid[i+1][j]);
-            } else if ( j < columnCount - 1) {
-                grid[i][j] = grid[i][j] + grid[i][j+1]
-            } else if (i < rowCount - 1){
-                grid[i][j] = grid[i][j] + grid[i+1][j]
-            } else {
-                grid[i][j] = grid[i][j];
-            }
-        }
+var minPathSum = function (grid) {
+  let rowCount = grid.length;
+  let columnCount = rowCount > 0 ? grid[0].length : 0;
+  for (let i = rowCount - 1; i >= 0; i--) {
+    for (let j = columnCount - 1; j >= 0; j--) {
+      if (j < columnCount - 1 && i < rowCount - 1) {
+        grid[i][j] = grid[i][j] + Math.min(grid[i][j + 1], grid[i + 1][j]);
+      } else if (j < columnCount - 1) {
+        grid[i][j] = grid[i][j] + grid[i][j + 1];
+      } else if (i < rowCount - 1) {
+        grid[i][j] = grid[i][j] + grid[i + 1][j];
+      } else {
+        grid[i][j] = grid[i][j];
+      }
     }
+  }
 
-    return grid.length > 0 ? grid[0].length > 0 ? grid[0][0] : 0 : 0;
+  return grid.length > 0 ? (grid[0].length > 0 ? grid[0][0] : 0) : 0;
 };
 
-console.log(minPathSum([[5,0,1,1,2,1,0,1,3,6,3,0,7,3,3,3,1],[1,4,1,8,5,5,5,6,8,7,0,4,3,9,9,6,0],[2,8,3,3,1,6,1,4,9,0,9,2,3,3,3,8,4],[3,5,1,9,3,0,8,3,4,3,4,6,9,6,8,9,9],[3,0,7,4,6,6,4,6,8,8,9,3,8,3,9,3,4],[8,8,6,8,3,3,1,7,9,3,3,9,2,4,3,5,1],[7,1,0,4,7,8,4,6,4,2,1,3,7,8,3,5,4],[3,0,9,6,7,8,9,2,0,4,6,3,9,7,2,0,7],[8,0,8,2,6,4,4,0,9,3,8,4,0,4,7,0,4],[3,7,4,5,9,4,9,7,9,8,7,4,0,4,2,0,4],[5,9,0,1,9,1,5,9,5,5,3,4,6,9,8,5,6],[5,7,2,4,4,4,2,1,8,4,8,0,5,4,7,4,7],[9,5,8,6,4,4,3,9,8,1,1,8,7,7,3,6,9],[7,2,3,1,6,3,6,6,6,3,2,3,9,9,4,4,8]]));
+console.log(
+  minPathSum([
+    [5, 0, 1, 1, 2, 1, 0, 1, 3, 6, 3, 0, 7, 3, 3, 3, 1],
+    [1, 4, 1, 8, 5, 5, 5, 6, 8, 7, 0, 4, 3, 9, 9, 6, 0],
+    [2, 8, 3, 3, 1, 6, 1, 4, 9, 0, 9, 2, 3, 3, 3, 8, 4],
+    [3, 5, 1, 9, 3, 0, 8, 3, 4, 3, 4, 6, 9, 6, 8, 9, 9],
+    [3, 0, 7, 4, 6, 6, 4, 6, 8, 8, 9, 3, 8, 3, 9, 3, 4],
+    [8, 8, 6, 8, 3, 3, 1, 7, 9, 3, 3, 9, 2, 4, 3, 5, 1],
+    [7, 1, 0, 4, 7, 8, 4, 6, 4, 2, 1, 3, 7, 8, 3, 5, 4],
+    [3, 0, 9, 6, 7, 8, 9, 2, 0, 4, 6, 3, 9, 7, 2, 0, 7],
+    [8, 0, 8, 2, 6, 4, 4, 0, 9, 3, 8, 4, 0, 4, 7, 0, 4],
+    [3, 7, 4, 5, 9, 4, 9, 7, 9, 8, 7, 4, 0, 4, 2, 0, 4],
+    [5, 9, 0, 1, 9, 1, 5, 9, 5, 5, 3, 4, 6, 9, 8, 5, 6],
+    [5, 7, 2, 4, 4, 4, 2, 1, 8, 4, 8, 0, 5, 4, 7, 4, 7],
+    [9, 5, 8, 6, 4, 4, 3, 9, 8, 1, 1, 8, 7, 7, 3, 6, 9],
+    [7, 2, 3, 1, 6, 3, 6, 6, 6, 3, 2, 3, 9, 9, 4, 4, 8],
+  ])
+);
